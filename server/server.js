01698a73504be46ef4a04a76e626774efaf8b610
Pere <pere.solaclaver@gmail.com>
2019-12-01 21:56:25 +0000

    fix path

--- server/server.js
+++ server/server.js
@@ -3,13 +3,13 @@ const cors = require('cors');
 
 const app = express();
 
-app.use(express.static(__dirname + '/client/build'));
+app.use(express.static(__dirname + '../client/build'));
 app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
 //   res.status(200).json({message: 'hey'})
-  res.sendFile(__dirname + '/client/build/index.html');
+  res.sendFile(__dirname + '../client/build/index.html');
 });
 
 module.exports = app;
